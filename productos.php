62ae439ac4325ca5c16705741c629e39cec9dde1
chrisboror <2019-40079@liceocanadiense.edu.gt>
2021-08-19 17:44:54 -0600

    actualizando carpeta

---
+++ productos.php
@@ -0,0 +1,230 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <title>Productos/Servicios</title>
+    <link rel="stylesheet" href="css.css">
+  
+  </head>
+  <body>
+ <br><br>
+
+    </div>
+    <div class="container">
+        <div class="btn-group w-100" role="group" aria-label="Basic exaple">
+        <a href="index.html" class="btn btn-dark">Inicio</a>
+        <a href="tiendas.php" class="btn btn-dark">Tiendas</a>
+        <a href="contactenos.php" class="btn btn-dark">Contactenos</a>
+        </div>
+    </div>
+    <br><br>
+  
+    <div class="container">
+       <div style="color: antiquewhite;">
+        <h3>Productos/Servicios</h3>
+     </div>
+     <br><br>
+      <div class="container">
+    <div class="container">
+        <div class="row">
+
+          <div class="col-3" align="center">
+              <div class="card" style="width: 15rem; background-color:#090909">
+                <img src="imagenes/vans.jpg" class="card-img-top" height="237">
+                <div class="card-body">
+                 <div style="color: antiquewhite;">
+                    <h5 class="card-title">Vans Old School</h5>
+                    <p class="card-text"><b>Precio: </b>Q.307.92</p>
+                </div><br>
+                <div class="btn btn-dark">Comprar</div>
+                </div>
+              </div>
+          </div><br>
+
+          <div class="col-3" align="center">
+              <div class="card" style="width: 15rem; background-color:#090909">
+                <img src="imagenes/mocasinas.jpg" class="card-img-top" height="237">
+                <div class="card-body">
+                 <div style="color: antiquewhite;">
+                    <h5 class="card-title">Mocasinas Oxford</h5>
+                    <p class="card-text"><b>Precio: </b>Q.600.90</p>
+                </div><br>
+                <div class="btn btn-dark">Comprar</div>
+                </div>
+              </div>
+          </div><br>
+
+          <div class="col-3" align="center">
+              <div class="card" style="width: 15rem; background-color:#090909">
+                <img src="imagenes/air.jpg" class="card-img-top" height="237">
+                <div class="card-body">
+                 <div style="color: antiquewhite;">
+                    <h5 class="card-title">Nike Air Force 1</h5>
+                    <p class="card-text"><b>Precio: </b>Q.500.82</p>
+                </div><br>
+                <div class="btn btn-dark">Comprar</div>
+                </div>
+              </div>
+          </div><br>
+
+          <div class="col-3" align="center">
+              <div class="card" style="width: 15rem; background-color:#090909">
+                <img src="imagenes/yeezy.jpg" class="card-img-top" height="237">
+                <div class="card-body">
+                 <div style="color: antiquewhite;">
+                    <h5 class="card-title">Adidas Yeezy 350 v2</h5>
+                    <p class="card-text"><b>Precio: </b>Q.1095.92</p>
+                </div><br>
+                <div class="btn btn-dark">Comprar</div>
+                </div>
+              </div>
+          </div><br>
+        </div>
+    </div>
+</div>
+<br><br><br>
+
+<div class="container">
+    <div class="container">
+        <div class="row">
+
+          <div class="col-3" align="center">
+              <div class="card" style="width: 15rem; background-color:#090909">
+                <img src="imagenes/700.jpg" class="card-img-top" height="237">
+                <div class="card-body">
+                 <div style="color: antiquewhite;">
+                    <h5 class="card-title">Adidas Yeezy 700</h5>
+                    <p class="card-text"><b>Precio: </b>Q.1120.92</p>
+                </div><br>
+                <div class="btn btn-dark">Comprar</div>
+                </div>
+              </div>
+          </div><br>
+
+          <div class="col-3" align="center">
+              <div class="card" style="width: 15rem; background-color:#090909">
+                <img src="imagenes/balen.jpg" class="card-img-top" height="237">
+                <div class="card-body">
+                 <div style="color: antiquewhite;">
+                    <h5 class="card-title">Balenciaga Track 2</h5>
+                    <p class="card-text"><b>Precio: </b>Q.3000.00</p>
+                </div><br>
+                <div class="btn btn-dark">Comprar</div>
+                </div>
+              </div>
+          </div><br>
+
+          <div class="col-3" align="center">
+              <div class="card" style="width: 15rem; background-color:#090909">
+                <img src="imagenes/ciaga.jpg" class="card-img-top" height="237">
+                <div class="card-body">
+                 <div style="color: antiquewhite;">
+                    <h5 class="card-title">Balenciaga Triple S</h5>
+                    <p class="card-text"><b>Precio: </b>Q.5000.00</p>
+                </div><br>
+                <div class="btn btn-dark">Comprar</div>
+                </div>
+              </div>
+          </div><br>
+
+          <div class="col-3" align="center">
+              <div class="card" style="width: 15rem; background-color:#090909">
+                <img src="imagenes/j.jpg" class="card-img-top" height="237">
+                <div class="card-body">
+                 <div style="color: antiquewhite;">
+                    <h5 class="card-title">Air Jordan 1 MID</h5>
+                    <p class="card-text"><b>Precio: </b>Q.850.92</p>
+                </div><br>
+                <div class="btn btn-dark">Comprar</div>
+                </div>
+              </div>
+          </div><br>
+        </div>
+    </div>
+</div>
+<br><br><br>
+
+<div class="container">
+    <div class="container">
+        <div class="row">
+
+          <div class="col-3" align="center">
+              <div class="card" style="width: 15rem; background-color:#090909">
+                <img src="imagenes/270.jpg" class="card-img-top" height="237">
+                <div class="card-body">
+                 <div style="color: antiquewhite;">
+                    <h5 class="card-title">Nike Air Max 270</h5>
+                    <p class="card-text"><b>Precio: </b>Q.789.92</p>
+                </div><br>
+                <div class="btn btn-dark">Comprar</div>
+                </div>
+              </div>
+          </div><br>
+
+          <div class="col-3" align="center">
+              <div class="card" style="width: 15rem; background-color:#090909">
+                <img src="imagenes/fly.jpg" class="card-img-top" height="237">
+                <div class="card-body">
+                 <div style="color: antiquewhite;">
+                    <h5 class="card-title">Nike Go Flyease</h5>
+                    <p class="card-text"><b>Precio: </b>Q.400.92</p>
+                </div><br>
+                  <div class="btn btn-dark">Comprar</div>
+                </div>
+              </div>
+          </div><br>
+
+          <div class="col-3" align="center">
+              <div class="card" style="width: 15rem; background-color:#090909">
+                <img src="imagenes/m.jpg" class="card-img-top" height="237">
+                <div class="card-body">
+                 <div style="color: antiquewhite;">
+                    <h5 class="card-title">Nike Air Monarch </h5>
+                    <p class="card-text"><b>Precio: </b>Q.552.92</p>
+                </div><br>
+                <div class="btn btn-dark">Comprar</div>
+                </div>
+              </div>
+          </div><br>
+
+          <div class="col-3" align="center">
+              <div class="card" style="width: 15rem; background-color:#090909">
+                <img src="imagenes/max.jpg" class="card-img-top" height="237">
+                <div class="card-body">
+                 <div style="color: antiquewhite;">
+                    <h5 class="card-title">Nike Air Max 97</h5>
+                    <p class="card-text"><b>Precio: </b>Q.1302.92</p>
+                </div><br>
+                <div class="btn btn-dark">Comprar</div>
+                </div>
+              </div>
+          </div><br>
+        </div>
+    </div>
+</div>
+<br><br><br>
+
+        
+<table class="table table-hover">
+    <thead>
+        <tr class="table-dark">
+            <td>PBX:40321781         DIRECCION:FUENTES DEL VALLE 1,ZONA 7, SAN MIGUEL PETAPA</td>
+            <td>CARNET:2019-40079   NOMBRE:CHRISTOPHER BOROR</td>
+        </tr>
+    </tbody>
+</table>
+
+
+    
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+
+
+
+  </body>
+ 
+</html>
