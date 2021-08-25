b4003f7752da404616dff645216b66601b1ccec7
chrisboror <2019-40079@liceocanadiense.edu.gt>
2021-08-25 16:42:32 -0600

    se actualizo el repositorio

---
+++ tiendas.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <title>Productos/Servicios</title>
+    <link rel="stylesheet" href="css.css">
+  
+  </head>
+  <body>
+ <br><br>
+
+    </div>
+    <div class="container">
+        <div class="btn-group w-100" role="group" aria-label="Basic exaple">
+        <a href="index.html" class="btn btn-dark">Inicio</a>
+        <a href="tiendas.php" class="btn btn-dark">Tiendas</a>
+        <a href="contactenos.php" class="btn btn-dark">Contactenos</a>
+        </div>
+    </div>
+    <br><br>
+
+    <div class="container">
+       <div style="color: antiquewhite;">
+        <h3>Nuestras Tiendas</h3>
+     </div>
+     <br><br>
+
+ <div class="container">
+      <div class="container">
+          <div class="row" style="color: antiquewhite;">
+              <img src="imagenes/Sperry1.jpg" style="height: 400px; width: 400px;">
+              <div class="col-6"><br><br><br><h3>Sperry Cayala</h3>
+              <br>  <ul><li>48985478</ul>
+              <br>  <ul><li>Blvd. Rafael Landivar 10-05 Zona 16, local 1001</ul>
+              <br>  <ul><li>Sperrycayala@gmail.com</ul>
+              <br>  <ul><li>8:00 am - 18:00 pm</ul>
+          </div>
+
+         <br><br>
+         <div class="row" style="color: antiquewhite;">
+              <img src="imagenes/Sperry2.jpg" style="height: 400px; width: 400px; margin-top: 50px;">
+              <div class="col-6"><br><br><br><br><br><h3>Sperry Miraflores</h3>
+              <br>  <ul><li>65854121</ul>
+              <br>  <ul><li>21 Avenida 4-32, zona 11 local 50</ul>
+              <br>  <ul><li>Sperrymiraflores@gmail.com</ul>
+              <br>  <ul><li>9:00 am - 17:00 pm</ul>
+          </div>
+
+          <br><br>
+          <div class="row" style="color: antiquewhite;">
+              <img src="imagenes/Sperry3.jpg" style="height: 400px; width: 400px; margin-top: 50px;">
+              <div class="col-6"><br><br><br><br><br><h3>Sperry Majadas</h3>
+              <br>  <ul><li>25495478</ul>
+              <br>  <ul><li>27 Avenida, 6-40 zona 11, local 22</ul>
+              <br>  <ul><li>Sperrymajadas@gmail.com</ul>
+              <br>  <ul><li>9:00 am - 16:00 pm</ul>
+          </div>
+        </div>
+    </div>
+
+          <br><br><br>
+          
+              <table class="table table-hover">
+                   <thead>
+                    <tr class="table-dark">
+                    <td>PBX:40321781         DIRECCION:FUENTES DEL VALLE 1,ZONA 7, SAN MIGUEL PETAPA</td>
+                    <td>CARNET:2019-40079   NOMBRE:CHRISTOPHER BOROR</td>
+                    </tr>
+                   </tbody>
+              </table>
+           
+
+
+
+   
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+</body>
+</html>
